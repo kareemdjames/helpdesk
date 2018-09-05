b57aeb83f3f9a5c974bdca97c358b44c896923c7
Kareem James <kareemdjames@gmail.com>
2018-09-04 23:21:50 -0400

    created initial nav component (still needs to add some things)

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div
+        className="navbar navbar-dark bg-dark navbar-expand-lg"
+        role="navigation"
+      >
+        <Link to="/" className="navbar-brand" href="#">
+          Help Desk
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav mr-auto">
+            <Link className="nav-item nav-link active" to="/">
+              Home
+            </Link>
+            <Link className="nav-item nav-link" to="/view-ticket">
+              Tickets
+            </Link>
+            <Link className="nav-item nav-link" to="/add-ticket">
+              Add new ticket
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
