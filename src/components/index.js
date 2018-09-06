62926c47b9b1e45fab090c9dd20b8b7a27afd926
Kareem James <kareemdjames@gmail.com>
2018-09-06 09:43:31 -0400

    added home component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Nav from './nav/Nav';
 import AddTicket from './add-ticket/AddTicket';
 import ViewTickets from './tickets-listing/ViewTickets';
+import Login from './login/Login';
+import Home from './home/Home';
 
-export { Nav, AddTicket, ViewTickets };
+export { Nav, AddTicket, ViewTickets, Login, Home };
