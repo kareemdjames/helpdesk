4fc0c1eae35b0a72e75621f6d0ca0baf8fb4c521
Kareem James <kareemdjames@gmail.com>
2018-09-05 20:51:26 -0400

    created add ticket component

---
+++ src/components/add-ticket/AddTicket.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AddTicketForm from './AddTicketForm';
+
+class AddTicket extends Component {
+  render() {
+    return <AddTicketForm />;
+  }
+}
+
+export default AddTicket;
