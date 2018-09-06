60793eef7e379260eb20fbd1a2900ecf80cdfc64
Kareem James <kareemdjames@gmail.com>
2018-09-05 21:54:50 -0400

    created firebase config

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: 'AIzaSyArZ8Nw-wBip7a2Lgszu9QoK3OEOw5iECc',
+  authDomain: 'swarf-eb.firebaseapp.com',
+  databaseURL: 'https://swarf-eb.firebaseio.com',
+  projectId: 'swarf-eb',
+  storageBucket: 'swarf-eb.appspot.com',
+  messagingSenderId: '946602163393'
+};
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+export const auth = firebase.auth();
