90fc085d03bce13ed63b9bb755ce517515983119
Kareem James <kareemdjames@gmail.com>
2018-09-06 09:43:40 -0400

    created initial home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello</h1>
+      </Fragment>
+    );
+  }
+}
