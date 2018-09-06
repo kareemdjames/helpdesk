97f88e6ccf080a71d05d492b9d392aa2af8f7543
Kareem James <kareemdjames@gmail.com>
2018-09-05 22:58:00 -0400

    added view ticket component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Nav from './nav/Nav';
 import AddTicket from './add-ticket/AddTicket';
+import ViewTickets from './tickets-listing/ViewTickets';
 
-export { Nav, AddTicket };
+export { Nav, AddTicket, ViewTickets };
