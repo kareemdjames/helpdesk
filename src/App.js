e1851138410cd1e92feb3fd4f98ae6a4885f9773
Kareem James <kareemdjames@gmail.com>
2018-09-06 09:43:25 -0400

    reconfigured to add auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Nav, AddTicket, ViewTickets } from './components/index';
+import { Nav, AddTicket, ViewTickets, Login, Home } from './components/index';
 
 class App extends Component {
   constructor(props) {
@@ -14,16 +14,33 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Fragment>
-          <Route path="/" component={Nav} />
-          <div className="container">
-            <Route path="/add-ticket" component={AddTicket} />
-            <Route path="/view-ticket" component={ViewTickets} />
-          </div>
-        </Fragment>
+        <div className="container">
+          {this.state.authenticated ? (
+            <Fragment>
+              <Nav authenticated={this.state.authenticated} />
+              <Route
+                path="/"
+                render={() => <Home userInfo={this.state.data} />}
+              />
+              <Route path="/view-ticket" component={ViewTickets} />
+              <Route path="/add-ticket" component={AddTicket} />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Nav authenticated={this.state.authenticated} />
+              <Route exact path="/login" component={Login} />
+            </Fragment>
+          )}
+        </div>
       </Router>
     );
   }
 }
 
 export default App;
+
+{
+  /* <Route path="/" component={Nav} />
+        <Route path="/add-ticket" component={AddTicket} />
+        <Route path="/view-ticket" component={ViewTickets} /> */
+}
