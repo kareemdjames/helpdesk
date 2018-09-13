87c5c48246ca70a93e95fbf7187f6f91ad47602e
Kareem James <kareemdjames@gmail.com>
2018-09-13 02:14:25 -0400

    able to render user profile to home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,25 @@ import React, { Component, Fragment } from 'react';
 
 class Home extends Component {
   render() {
+    let props = this.props;
+    let userPhoto = { width: '80px', height: '80px', marginTop: '10px' };
     return (
       <Fragment>
-        <h1>Hello</h1>
+        {props.userInfo.map(profile => {
+          return (
+            <Fragment key={profile.uid}>
+              <h2>{profile.displayName} - Welcome to HelpDesk Application</h2>
+              <div style={userPhoto}>
+                <img src={profile.photoURL} alt="user" />
+                <br />
+                <span>
+                  <b>Email:</b>
+                </span>
+                {profile.email}
+              </div>
+            </Fragment>
+          );
+        })}
       </Fragment>
     );
   }
