c3b2cb8c2112c07a4461c7c0f30430470ae7e120
Kareem James <kareemdjames@gmail.com>
2018-09-13 02:40:22 -0400

    created logout component

---
+++ src/components/logout/Logout.js
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import { firebaseApp } from '../../firebase';
+
+class Logout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: props.authenticated,
+      data: ''
+    };
+  }
+
+  componentWillMount() {
+    firebaseApp
+      .auth()
+      .signOut()
+      .then(user => {
+        this.setState({
+          redirect: true,
+          data: null
+        });
+      });
+  }
+
+  render() {
+    if (this.state.redirect === true) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div
+        style={{
+          textAlign: 'center',
+          position: 'absolute',
+          top: '25%',
+          left: '50%'
+        }}
+      >
+        <h4>Logging out...</h4>
+      </div>
+    );
+  }
+}
+
+export default Logout;
